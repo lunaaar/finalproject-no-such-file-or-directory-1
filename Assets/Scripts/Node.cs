10d7813cec143a35b512c26e8b24142d900cbefa
John Li <lij19@wit.edu>
2022-04-13 14:29:11 -0400

    hovering over node causes the color to change to red and color changes back when mouse leaves

---
+++ Assets/Scripts/Node.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Node : MonoBehaviour
+{
+    SpriteRenderer sprite;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        sprite = GetComponent<SpriteRenderer>();
+    }
+
+    private void OnMouseEnter()
+    {
+        sprite.color = new Color(1, 0, 0, 1);
+        //print("hi");
+    }
+
+    private void OnMouseExit()
+    {
+        sprite.color = new Color(0.2235294f, 0.4039216f, 0.2392157f, 1);
+    }
+}
