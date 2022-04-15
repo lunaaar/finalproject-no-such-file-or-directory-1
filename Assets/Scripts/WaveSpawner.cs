a7d64367d7767caee1452c64761d6a30c1f92821
Samuel Rubin <sjrube32@gmail.com>
2022-04-15 14:01:22 -0400

    Round button

--- Assets/Scripts/WaveSpawner.cs
+++ Assets/Scripts/WaveSpawner.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using UnityEngine.UI;
 using UnityEngine;
 
 public class WaveSpawner : MonoBehaviour
@@ -15,6 +16,8 @@ public class WaveSpawner : MonoBehaviour
     private int currentRound;
     public float countdown;
     public float timeBetweenWaves;
+
+    public Text roundText;
 
     public WaveSpawner(Enemy d, Enemy t, Enemy n, Enemy p, Enemy o, Enemy r)
     {
@@ -33,22 +36,72 @@ public class WaveSpawner : MonoBehaviour
 
     void Update()
     {
-        if (countdown <= 0)
-        {
-            StartCoroutine(spawnWave());
-            countdown = timeBetweenWaves;
-
-        }
 
         countdown -= Time.deltaTime;
     }
 
+    public void callWave()
+    {
+        if (countdown <= 0)
+        {
+            countdown = timeBetweenWaves;
+            StartCoroutine(spawnWave());
+        }
+        
+    }
+
     IEnumerator spawnWave()
     {
+        roundText.text = currentRound.ToString();
         for (int i = 0; i < currentRound; i++)
         {
             spawn(ddosEnemyPrefab);
-            yield return new WaitForSeconds(0.5f);
+            yield return new WaitForSeconds(0.2f);
+        }
+
+        if (currentRound > 3)
+        {
+            for (int i = 0; i < Mathf.Floor(currentRound / 2); i++)
+            {
+                spawn(trojanPrefab);
+                yield return new WaitForSeconds(0.2f);
+            }
+        }
+
+        if (currentRound > 7)
+        {
+            for (int i = 0; i < Mathf.Floor(Mathf.Sqrt(currentRound)); i++)
+            {
+                spawn(networkPrefab);
+                yield return new WaitForSeconds(0.2f);
+            }
+        }
+
+        if (currentRound > 10)
+        {
+            for (int i = 0; i < Mathf.Floor(currentRound / 5); i++)
+            {
+                spawn(polymorphicPrefab);
+                yield return new WaitForSeconds(0.2f);
+            }
+        }
+
+        if (currentRound > 15)
+        {
+            for (int i = 0; i < Mathf.Floor(currentRound / 15); i++)
+            {
+                spawn(overwritePrefab);
+                yield return new WaitForSeconds(0.2f);
+            }
+        }
+
+        if (currentRound > 20)
+        {
+            for (int i = 0; i < Mathf.Floor(currentRound / 4); i++)
+            {
+                spawn(residentPrefab);
+                yield return new WaitForSeconds(0.2f);
+            }
         }
 
         currentRound++;
